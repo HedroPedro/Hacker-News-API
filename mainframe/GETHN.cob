193c06c4c2dbc812595f6926ca2146b242a785a7
HedroPedro <pedrodominador05@gmail.com>
2026-06-27 16:59:23 -0300

    fix: bugs and faulty features

--- mainframe/GETHN.cob
+++ mainframe/GETHN.cob
@@ -1,4 +1,4 @@
-       IDENTIFICATION DIVISION. 
+       IDENTIFICATION DIVISION.
        PROGRAM-ID. GETHN.
        AUTHOR. PEDRO.
        ENVIRONMENT DIVISION.
@@ -12,8 +12,8 @@
               FILE STATUS IS VSAM-FLAGS.
        DATA DIVISION.
        FILE SECTION.
-       FD JSON-FD.
-       01 JSON-DATA PIC X(10000).
+       FD JSON-FD RECORDING MODE IS F.
+       01 JSON-DATA PIC X(1500).
        FD CLSTER-FD.
        01 CLSTER-REC.
            05 CLSTER-KEY     PIC X(6).
@@ -26,8 +26,8 @@
            05 ANY-VAL        PIC X VALUE 'N'.
        01  JSON-REC.
            05 TYPE-INFO      PIC X(5).
-           05 TOTAL-SCORE    PIC 9(11).
-           05 TOTAL-COMMENTS PIC 9(11).
+           05 TOTAL-SCORE    PIC 9(18) VALUE ZERO.
+           05 TOTAL-COMMENTS PIC 9(18) VALUE ZERO.
            05 AVG-SCORE      PIC 9(11)V99 VALUE ZERO.
            05 AVG-COMMENT    PIC 9(11)V99 VALUE ZERO.
        01 ERROR-JSON.
@@ -41,10 +41,9 @@
        01 WS-COUNTERS.
            05 WS-YEAR-CTR  PIC 9(4).
            05 WS-MON-CTR   PIC 9(2).
-           05 WS-TOTAL-CTR PIC 9(4).  
-       01 WS-TEMP-VALS VALUE ZERO.
-           05 TMP-AVG-SCORE    PIC 9(22).
-           05 TMP-AVG-COMMENTS PIC 9(22).
+           05 WS-TOTAL-CTR PIC 9(4).
+       01 WS-TMP-STR  PIC X(80).
+       01 WS-CURRENT-YEAR PIC 9(4).
        LINKAGE SECTION.
        01 PARMDATA.
            05 STRINGLEN PIC  S9(4) USAGE COMP.
@@ -53,35 +52,37 @@
        000-MAIN.
            OPEN OUTPUT JSON-FD.
            OPEN INPUT CLSTER-FD.
+           DISPLAY STRINGPARM.
            PERFORM PROCESS-ARGS.
        010-END.
            CLOSE JSON-FD.
            CLOSE CLSTER-FD.
            GOBACK.
-      * FUNCTIONS      
+      * FUNCTIONS
        PROCESS-ARGS.
            IF STRINGLEN EQUAL ZERO
               MOVE 'MUST HAVE YEAR OR MONTH' TO REASON
               MOVE 500 TO ERR-CODE
               PERFORM GEN-ERROR-JSON
               EXIT PARAGRAPH
-           END-IF. 
+           END-IF.
            IF STRINGPARM(1:5) EQUAL TO 'YEAR='
               SUBTRACT 5 FROM STRINGLEN
               IF STRINGLEN NOT EQUAL TO 4
                  MOVE 'YEAR MUST HAVE 4 DIGITS' TO REASON
                  MOVE 500 TO ERR-CODE
-                 PERFORM GEN-ERROR-JSON 
+                 PERFORM GEN-ERROR-JSON
                  EXIT PARAGRAPH
               END-IF
-              MOVE STRINGPARM(6:4) TO STRINGPARM
-              IF STRINGPARM IS NOT NUMERIC
+              MOVE STRINGPARM(6:4) TO WS-TMP-STR
+              DISPLAY WS-TMP-STR
+              IF WS-TMP-STR(1:4) IS NOT NUMERIC
                  MOVE 'EXPECTED NUMBERS' TO REASON
                  MOVE 500 TO ERR-CODE
                  PERFORM GEN-ERROR-JSON
                  EXIT PARAGRAPH
               END-IF
-              MOVE STRINGPARM(1:4) TO WS-YEAR 
+              MOVE WS-TMP-STR(1:4) TO WS-YEAR
               PERFORM GET-ALL-SAME-YEAR
               EXIT PARAGRAPH
            END-IF.
@@ -93,14 +94,14 @@
                  PERFORM GEN-ERROR-JSON
                  EXIT PARAGRAPH
               END-IF
-              MOVE STRINGPARM(7:2) TO STRINGPARM
-              IF STRINGPARM IS NOT NUMERIC
+              MOVE STRINGPARM(7:2) TO WS-TMP-STR
+              IF WS-TMP-STR(1:2) IS NOT NUMERIC
                  MOVE 'EXPECTED NUMBERS' TO REASON
                  MOVE 500 TO ERR-CODE
                  PERFORM GEN-ERROR-JSON
                  EXIT PARAGRAPH
               END-IF
-              MOVE STRINGPARM(1:2) TO WS-MON
+              MOVE WS-TMP-STR(1:2) TO WS-MON
               PERFORM GET-ALL-SAME-MON
               EXIT PARAGRAPH
            END-IF
@@ -112,39 +113,41 @@
                  PERFORM GEN-ERROR-JSON
                  EXIT PARAGRAPH
               END-IF
-              MOVE STRINGPARM(11:6) TO STRINGPARM
-              IF STRINGPARM IS NOT NUMERIC
+              MOVE STRINGPARM(11:6) TO WS-TMP-STR
+              IF WS-TMP-STR(1:6) IS NOT NUMERIC
                  MOVE 'EXPECTED NUMBERS' TO REASON
                  MOVE 500 TO ERR-CODE
                  PERFORM GEN-ERROR-JSON
                  EXIT PARAGRAPH
               END-IF
-              MOVE STRINGPARM(1:6) TO CLSTER-KEY
+              MOVE WS-TMP-STR(1:6) TO CLSTER-KEY
               PERFORM GET-ID
               EXIT PARAGRAPH
            END-IF.
-           MOVE 'INVALID ARGUMENT' TO ERROR-JSON.
+           MOVE 'INVALID ARGUMENT' TO REASON.
            MOVE 500 TO ERR-CODE.
            PERFORM GEN-ERROR-JSON.
        GEN-JSON-GENERIC.
-           JSON GENERATE JSON-DATA FROM JSON-REC.
+           JSON GENERATE JSON-DATA FROM JSON-REC
+              NAME OF JSON-REC IS OMITTED.
            WRITE JSON-DATA.
        GEN-ERROR-JSON.
-           JSON GENERATE JSON-DATA FROM ERROR-JSON.
+           JSON GENERATE JSON-DATA FROM ERROR-JSON
+              NAME OF ERROR-JSON IS OMITTED.
            WRITE JSON-DATA.
        READ-VSAM.
            READ CLSTER-FD
               INVALID KEY CONTINUE
               NOT INVALID KEY
                  MOVE 'Y' TO ANY-VAL
-                 ADD CLSTER-SCORE TO TMP-AVG-SCORE
-                 ADD CLSTER-COMMENT TO TMP-AVG-COMMENTS
+                 ADD CLSTER-SCORE TO TOTAL-SCORE
+                 ADD CLSTER-COMMENT TO TOTAL-COMMENTS
                  ADD 1 TO WS-TOTAL-CTR
            END-READ.
        CALC-AVG.
-           DIVIDE TMP-AVG-SCORE BY WS-TOTAL-CTR 
+           DIVIDE TOTAL-SCORE BY WS-TOTAL-CTR
               GIVING AVG-SCORE ROUNDED.
-           DIVIDE TMP-AVG-COMMENTS BY WS-TOTAL-CTR 
+           DIVIDE TOTAL-COMMENTS BY WS-TOTAL-CTR
               GIVING AVG-COMMENT ROUNDED.
        GET-ALL-SAME-YEAR.
            PERFORM VARYING WS-MON-CTR
@@ -164,11 +167,14 @@
               PERFORM GEN-JSON-GENERIC
            END-IF.
        GET-ALL-SAME-MON.
-           MOVE 2006 TO WS-YEAR
-           PERFORM UNTIL VSAM-FLAGS NOT EQUAL 0
+           MOVE FUNCTION CURRENT-DATE(1:4) TO WS-CURRENT-YEAR.
+           PERFORM VARYING WS-YEAR
+              FROM 2006 BY 1
+              UNTIL WS-YEAR > WS-CURRENT-YEAR
                MOVE WS-ID TO CLSTER-KEY
+               DISPLAY 'KEY: ' CLSTER-KEY
                PERFORM READ-VSAM
-               ADD 1 TO WS-YEAR
+               DISPLAY 'FLAGS: ' VSAM-FLAGS
            END-PERFORM.
            IF ANY-VAL EQUAL 'N'
               MOVE 'DATA NOT FOUND' TO REASON
@@ -186,10 +192,11 @@
               MOVE 404 TO ERR-CODE
               PERFORM GEN-ERROR-JSON
            ELSE
-              MOVE 'ID' TO TYPE-INFO IN JSON-REC 
+              MOVE 'ID' TO TYPE-INFO IN JSON-REC
               MOVE CLSTER-SCORE TO TOTAL-SCORE
               MOVE CLSTER-COMMENT TO TOTAL-COMMENTS
               JSON GENERATE JSON-DATA FROM JSON-REC
+                 NAME OF JSON-REC IS OMITTED
                  SUPPRESS AVG-SCORE AVG-COMMENT
               END-JSON
               WRITE JSON-DATA
